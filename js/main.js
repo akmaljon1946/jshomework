d4d972057f7aae5ba843e1629f32362740a35d2a
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-04-20 16:31:09 +0500

    add imtixon

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+// let array = ["salom dunyo"];
+
+// console.log(array.reverse());
+
+let arr = ["i","study","JavaScript","right","now"];
+
+console.log(arr.slice()[2]);
+
+
+let number = 2;
+
+function num(number) {
+    if(number > 0)
+
+}
+
+num(1)
